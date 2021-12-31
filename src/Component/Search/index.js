a2bdf9a58e79d321b003dca3849ecc5d55e13029
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-12-31 10:23:58 +0700

    refactoring search component

--- src/Component/Search/index.js
+++ src/Component/Search/index.js
@@ -1,26 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HiSearch } from "react-icons/hi";
-import './style.css'
+import "./style.css";
 
 import TopNav from "../TopNav";
 
 function Search() {
   return (
     <TopNav>
-      <div className="top-nav-brand">
-        <Link to="/">
-          <img
-            src="https://assets.kitabisa.cc/images/logos/logogram__ktbs_white.png"
-            alt=""
-          />
-        </Link>
-      </div>
-      <div className="top-nav-search">
-        {/* <div className="input-search-wrapper"> */}
-        <input type="text" placeholder="Cari Campaign" />
-        <HiSearch className="input-search-icon" />
-        {/* </div> */}
+      <div className="search-wrapper">
+        <div className="top-nav-brand">
+          <Link to="/">
+            <img
+              src="https://assets.kitabisa.cc/images/logos/logogram__ktbs_white.png"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="top-nav-search">
+          {/* <div className="input-search-wrapper"> */}
+          <input type="text" placeholder="Cari Campaign" />
+          <HiSearch className="input-search-icon" />
+          {/* </div> */}
+        </div>
       </div>
     </TopNav>
   );
