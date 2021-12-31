8cb4ffc7112e195b28eb0410ec973141988a70d8
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-12-31 11:19:10 +0700

    make go back function working

--- src/Component/HeaderNav/index.js
+++ src/Component/HeaderNav/index.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "./style.css";
 
 import { HiArrowLeft } from "react-icons/hi";
 
 function HeaderNav({ title }) {
+  const history = useHistory();
+
   return (
     <div className="header-nav">
-      <button className="btn-back">
+      <button onClick={(e) => history.goBack()} className="btn-back">
         <HiArrowLeft className="icon" />
       </button>
       <span>{title}</span>
