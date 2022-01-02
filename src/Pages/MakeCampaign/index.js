5a27b657d11c0f29284fec72edc99457c5f0e01b
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-02 19:41:31 +0700

    make page for add campaign

--- src/Pages/MakeCampaign/index.js
+++ src/Pages/MakeCampaign/index.js
@@ -12,8 +12,50 @@ function MakeCampaignPage() {
         <TopNav>
           <HeaderNav title="Buat Campagin" />
         </TopNav>
-        <div className="main-page-wrapper make-campaign">
-          <h1>Make Campaign Page</h1>
+        <div className="main-page-wrapper">
+          <div className="make-campaign">
+            <div className="info-fundaiser-form">
+              <div className="campaign-form-controll">
+                <h3>Nama Penggalang Dana</h3>
+                <input
+                  type="text"
+                  className="info-form"
+                  placeholder="Nama Penggalang Dana"
+                />
+              </div>
+            </div>
+            <hr className="form-breaker" />
+            <div className="info-fundaiser-form">
+              <div className="campaign-form-controll mb-3">
+                <h3>Nama campaign</h3>
+                <input
+                  type="text"
+                  className="info-form"
+                  placeholder="Nama Campaign"
+                />
+              </div>
+              <div className="campaign-form-controll mb-3">
+                <h3>Target dana terkumpul</h3>
+                <input type="number" className="info-form" placeholder="Rp.." />
+              </div>
+              <div className="campaign-form-controll mb-3">
+                <h3>Lama galang dana berlangsung</h3>
+                <input type="date" className="info-form" placeholder="Rp.." />
+              </div>
+              <div className="campaign-form-controll mb-3">
+                <h3>Cerita</h3>
+                <textarea name=""></textarea>
+              </div>
+              <div className="campaign-form-controll">
+                <h3>Foto galang dana</h3>
+                <input type="file" className="info-form" placeholder="Rp.." />
+              </div>
+            </div>
+            <hr className="form-breaker" />
+            <button type="submit" className="btn btn-primary w-100">
+              Buat Campaign
+            </button>
+          </div>
         </div>
         <BottomNav />
       </div>
