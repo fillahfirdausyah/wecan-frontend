5116948ca50c8cd61c19b7a35217e1f6c33dd7d5
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-03 11:34:16 +0700

    make fundraiser info component for detail campaign page

---
+++ src/Component/CampaignDetail/FundraiserInfo/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+
+function FundraiserInfo() {
+  return (
+    <div className="fundraiser-container">
+      <h3>Informasi Penggalangan Dana</h3>
+      <div className="fundraiser-info-container">
+        <div className="fundraiser-title">Penggalang Dana</div> 
+        <div className="fundraiser-user-container">
+          <div className="fundraiser-avatar">
+            <img
+              src="https://i.pinimg.com/736x/12/1c/55/121c55b356c4dd0ad7a743b70770d323.jpg"
+              alt=""
+            />
+          </div>
+          <div className="fundraiser-info">
+            <div className="fundraiser-name-container">
+              <div className="fundraiser-name">
+                <p>Fillah Firdausyah</p>
+                <img
+                  src="https://assets.kitabisa.cc/images/icons/icon_verified.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+            <span className="fundraiser-status">Identitas terverifikasi</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FundraiserInfo;
