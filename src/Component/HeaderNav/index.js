5f25b3ca1139e02fa237bdfb2bb2858e06ec802e
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-03 10:41:19 +0700

    add style nav for campaign detail page

--- src/Component/HeaderNav/index.js
+++ src/Component/HeaderNav/index.js
@@ -4,16 +4,31 @@ import "./style.css";
 
 import { HiArrowLeft } from "react-icons/hi";
 
-function HeaderNav({ title }) {
+function HeaderNav({ title, campaignDetail }) {
   const history = useHistory();
 
   return (
-    <div className="header-nav">
-      <button onClick={(e) => history.goBack()} className="btn-back">
-        <HiArrowLeft className="icon" />
-      </button>
-      <span>{title}</span>
-    </div>
+    <>
+      {campaignDetail ? (
+        <>
+          <div className="header-nav detail-campaign">
+            <button onClick={(e) => history.goBack()} className="btn-back">
+              <HiArrowLeft className="icon" />
+            </button>
+            <span className="detail-campaign">{title}</span>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="header-nav">
+            <button onClick={(e) => history.goBack()} className="btn-back">
+              <HiArrowLeft className="icon" />
+            </button>
+            <span>{title}</span>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
