e80666a628c87764500af1552aa132d89a0e0c36
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-05 14:21:47 +0700

    make can display dynamic data

--- src/Component/CardCampaign/index.js
+++ src/Component/CardCampaign/index.js
@@ -1,24 +1,29 @@
 import React from "react";
 import "./style.css";
 
+import Moment from "react-moment";
+import "moment/locale/id";
+
+import CurrencyFormat from "react-currency-format";
 import ProgressBar from "@ramonak/react-progress-bar";
 
-function CardCampaign() {
+function CardCampaign({ data }) {
+  let current = (data.collected / data.goal) * 100;
+  let today = new Date();
+
   return (
     <>
       <div className="card-campaign">
         <figure>
           <div className="image-campaign-wrapper">
             <img
-              src="https://imgix.kitabisa.com/af55dd04-2e0c-44f6-98a1-1913fd64f0a2.jpg"
+              src={`http://localhost:8000/image/campaign/${data.cover}`}
               alt=""
             />
           </div>
         </figure>
         <div className="info-campagin-content">
-          <span className="title-campaign">
-            Pahala Mengalir Wakaf Air Untuk Santri Pelosok
-          </span>
+          <span className="title-campaign">{data.title}</span>
           <div className="info-fundraiser">
             <span>Sinergi Kebaikan Umat</span>
             <img
@@ -28,7 +33,7 @@ function CardCampaign() {
           </div>
           <div className="info-progressbar-campaign">
             <ProgressBar
-              completed={99}
+              completed={current}
               bgColor="#61dafb"
               customLabel=" "
               height="3px"
@@ -37,12 +42,24 @@ function CardCampaign() {
           <div className="list-count-campaign">
             <div className="dontation-collected">
               <span>Terkumpul</span>
-              <span className="balance">Rp 100.000.000</span>
+              {/* <span className="balance">{data.collected}</span> */}
+              <CurrencyFormat
+                decimalSeparator={""}
+                isNumericString={true}
+                value={data.collected}
+                displayType={"text"}
+                thousandSeparator="."
+                prefix={"Rp "}
+                renderText={(value) => <span className="balance">{value}</span>}
+              />
             </div>
             <div className="day-left-campaign">
               <span>Sisa hari</span>
               <span className="balance">
-                <strong>14</strong>
+                {/* <strong>14</strong> */}
+                <Moment diff={today} element="strong" unit="days" locale="id">
+                  {data.over}
+                </Moment>
               </span>
             </div>
           </div>
