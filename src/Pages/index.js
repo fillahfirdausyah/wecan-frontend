5357b77f5ba55bbc1c514688d971f62584e76a3f
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-05 09:31:23 +0700

    make exported component

---
+++ src/Pages/index.js
@@ -0,0 +1,23 @@
+import HomePage from "./Home";
+import LoginPage from "./Login";
+import UserPage from "./User";
+import CampaignPage from "./Campaign";
+import CampaignDetailPage from "./CampaignDetail";
+import MakeCampaignPage from "./MakeCampaign";
+import MyCampaignPage from "./MyCampaign";
+import DonationPage from "./Donation";
+import DonationHistoryPage from "./DonationHistory";
+import NotFoundPage from "./NotFound";
+
+export {
+  HomePage,
+  LoginPage,
+  UserPage,
+  CampaignPage,
+  CampaignDetailPage,
+  MakeCampaignPage,
+  MyCampaignPage,
+  DonationPage,
+  DonationHistoryPage,
+  NotFoundPage,
+};
