46a61c6f326e277742c37762f0cdf979c2c0dc49
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-05 20:42:55 +0700

    displat the wallet balance

--- src/Component/MenuItem/index.js
+++ src/Component/MenuItem/index.js
@@ -1,15 +1,32 @@
 import React from "react";
 import "./style.css";
 
+import CurrencyFormat from "react-currency-format";
+
 import { FaChevronRight } from "react-icons/fa";
 
-function MenuItem({ children, title, wallet }) {
+function MenuItem({ children, title, wallet, data }) {
   return (
     <div className="menu-item">
       {/* <IoWalletOutline className="menu-item-icon" /> */}
       {children}
       <span className="menu-name">{title}</span>
-      {wallet ? <span className="wallet-balance">Rp 1.000.000</span> : ""}
+      {/* {wallet ? <span className="wallet-balance">{data.balance}</span> : ""} */}
+      {wallet ? (
+        <CurrencyFormat
+          decimalSeparator={""}
+          isNumericString={true}
+          value={data.balance}
+          displayType={"text"}
+          thousandSeparator="."
+          prefix={"Rp "}
+          renderText={(value) => (
+            <span className="wallet-balance">{value}</span>
+          )}
+        />
+      ) : (
+        ""
+      )}
       <div className="btn-more">
         <FaChevronRight className="icon-more" />
       </div>
