860696d82b1c7cdd366a5dd705a9c385bbd83d9a
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-06 10:36:41 +0700

    make the button logout workings

--- src/Component/MenuItem/index.js
+++ src/Component/MenuItem/index.js
@@ -1,36 +1,76 @@
 import React from "react";
 import "./style.css";
 
+import { useAuth } from "../../Helpers/AuthContext";
+import { useHistory } from "react-router-dom";
+
 import CurrencyFormat from "react-currency-format";
 
 import { FaChevronRight } from "react-icons/fa";
 
-function MenuItem({ children, title, wallet, data }) {
+function MenuItem({ children, title, wallet, data, isLogout }) {
+  const { logout } = useAuth();
+  const history = useHistory();
+
+  const logOut = () => {
+    logout();
+    history.push("/");
+  };
+
   return (
-    <div className="menu-item">
-      {/* <IoWalletOutline className="menu-item-icon" /> */}
-      {children}
-      <span className="menu-name">{title}</span>
-      {/* {wallet ? <span className="wallet-balance">{data.balance}</span> : ""} */}
-      {wallet ? (
-        <CurrencyFormat
-          decimalSeparator={""}
-          isNumericString={true}
-          value={data.balance}
-          displayType={"text"}
-          thousandSeparator="."
-          prefix={"Rp "}
-          renderText={(value) => (
-            <span className="wallet-balance">{value}</span>
+    <>
+      {isLogout ? (
+        <div className="menu-item" onClick={logOut}>
+          {/* <IoWalletOutline className="menu-item-icon" /> */}
+          {children}
+          <span className="menu-name">{title}</span>
+          {/* {wallet ? <span className="wallet-balance">{data.balance}</span> : ""} */}
+          {wallet ? (
+            <CurrencyFormat
+              decimalSeparator={""}
+              isNumericString={true}
+              value={data.balance}
+              displayType={"text"}
+              thousandSeparator="."
+              prefix={"Rp "}
+              renderText={(value) => (
+                <span className="wallet-balance">{value}</span>
+              )}
+            />
+          ) : (
+            ""
           )}
-        />
+          <div className="btn-more">
+            <FaChevronRight className="icon-more" />
+          </div>
+        </div>
       ) : (
-        ""
+        <div className="menu-item">
+          {/* <IoWalletOutline className="menu-item-icon" /> */}
+          {children}
+          <span className="menu-name">{title}</span>
+          {/* {wallet ? <span className="wallet-balance">{data.balance}</span> : ""} */}
+          {wallet ? (
+            <CurrencyFormat
+              decimalSeparator={""}
+              isNumericString={true}
+              value={data.balance}
+              displayType={"text"}
+              thousandSeparator="."
+              prefix={"Rp "}
+              renderText={(value) => (
+                <span className="wallet-balance">{value}</span>
+              )}
+            />
+          ) : (
+            ""
+          )}
+          <div className="btn-more">
+            <FaChevronRight className="icon-more" />
+          </div>
+        </div>
       )}
-      <div className="btn-more">
-        <FaChevronRight className="icon-more" />
-      </div>
-    </div>
+    </>
   );
 }
 
