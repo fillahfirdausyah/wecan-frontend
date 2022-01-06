a7dea42ef31de9e079f73c29969b48754d9e22c7
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-06 10:35:23 +0700

    make function to verifiy the token

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Router from "./Route";
 import { AuthProvider } from "./Helpers/AuthContext";
 
+import api from "./Helpers/ApiEndpoint";
+
 function App() {
+  const history = useHistory();
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    const verifiyToken = async () => {
+      try {
+        let res = await api.get("/api/me", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      } catch (err) {
+        localStorage.removeItem("token");
+        history.push("/login");
+      }
+    };
+
+    verifiyToken();
+  }, []);
+
   return (
     <div className="App">
       <AuthProvider>
