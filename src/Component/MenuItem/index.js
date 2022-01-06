57cfb6a2e0b220f451f17968e1a5ab31eb6ec46e
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-06 14:19:35 +0700

    refactoring get function from parent component

--- src/Component/MenuItem/index.js
+++ src/Component/MenuItem/index.js
@@ -1,75 +1,35 @@
 import React from "react";
 import "./style.css";
 
-import { useAuth } from "../../Helpers/AuthContext";
-import { useHistory } from "react-router-dom";
-
 import CurrencyFormat from "react-currency-format";
 
 import { FaChevronRight } from "react-icons/fa";
 
-function MenuItem({ children, title, wallet, data, isLogout }) {
-  const { logout } = useAuth();
-  const history = useHistory();
-
-  const logOut = () => {
-    logout();
-    history.push("/");
-  };
-
+function MenuItem({ children, title, wallet, data, theFunction }) {
   return (
     <>
-      {isLogout ? (
-        <div className="menu-item" onClick={logOut}>
-          {/* <IoWalletOutline className="menu-item-icon" /> */}
-          {children}
-          <span className="menu-name">{title}</span>
-          {/* {wallet ? <span className="wallet-balance">{data.balance}</span> : ""} */}
-          {wallet ? (
-            <CurrencyFormat
-              decimalSeparator={""}
-              isNumericString={true}
-              value={data.balance}
-              displayType={"text"}
-              thousandSeparator="."
-              prefix={"Rp "}
-              renderText={(value) => (
-                <span className="wallet-balance">{value}</span>
-              )}
-            />
-          ) : (
-            ""
-          )}
-          <div className="btn-more">
-            <FaChevronRight className="icon-more" />
-          </div>
+      <div className="menu-item" onClick={theFunction}>
+        {children}
+        <span className="menu-name">{title}</span>
+        {wallet ? (
+          <CurrencyFormat
+            decimalSeparator={""}
+            isNumericString={true}
+            value={data.balance}
+            displayType={"text"}
+            thousandSeparator="."
+            prefix={"Rp "}
+            renderText={(value) => (
+              <span className="wallet-balance">{value}</span>
+            )}
+          />
+        ) : (
+          ""
+        )}
+        <div className="btn-more">
+          <FaChevronRight className="icon-more" />
         </div>
-      ) : (
-        <div className="menu-item">
-          {/* <IoWalletOutline className="menu-item-icon" /> */}
-          {children}
-          <span className="menu-name">{title}</span>
-          {/* {wallet ? <span className="wallet-balance">{data.balance}</span> : ""} */}
-          {wallet ? (
-            <CurrencyFormat
-              decimalSeparator={""}
-              isNumericString={true}
-              value={data.balance}
-              displayType={"text"}
-              thousandSeparator="."
-              prefix={"Rp "}
-              renderText={(value) => (
-                <span className="wallet-balance">{value}</span>
-              )}
-            />
-          ) : (
-            ""
-          )}
-          <div className="btn-more">
-            <FaChevronRight className="icon-more" />
-          </div>
-        </div>
-      )}
+      </div>
     </>
   );
 }
