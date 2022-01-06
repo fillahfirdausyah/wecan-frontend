60699bc74eb1ad9b7e52e3e1bbfb667119185721
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-01-06 15:57:46 +0700

    make topup wallet page

---
+++ src/Pages/TopupWallet/index.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+import api from "../../Helpers/ApiEndpoint";
+import { useHistory } from "react-router-dom";
+
+import TopNav from "../../Component/TopNav";
+import HeaderNav from "../../Component/HeaderNav";
+
+function TopupWalletPage() {
+  const token = localStorage.getItem("token");
+  const history = useHistory();
+  const [balance, setBalance] = useState("");
+
+  const topUpWallet = (e) => {
+    e.preventDefault();
+
+    api
+      .post(
+        "/api/wallet/topup",
+        {
+          balance: balance,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        history.push("/wallet");
+      });
+  };
+
+  return (
+    <div className="max-w-screen-sm">
+      <TopNav>
+        <HeaderNav title="Dompet Kebaikan" />
+      </TopNav>
+      <div className="main-page-wrapper">
+        <form onSubmit={topUpWallet}>
+          <div className="donation-amount-container">
+            <div className="donation-amount-wrapper">
+              <p>Masukan Nominal Topup</p>
+              <div className="donation-input-container">
+                <span>Rp</span>
+                <div className="donation-input-form">
+                  <input
+                    value={balance}
+                    onChange={(e) => setBalance(e.target.value)}
+                    type="number"
+                    className="donation-input"
+                    placeholder="0"
+                    name="amount"
+                  />
+                </div>
+              </div>
+              <p className="wallet-balance">Saldo dompet kebaikan:</p>
+            </div>
+          </div>
+          <div className="button-wrapper">
+            <button type="submit" className="btn btn-primary w-100">
+              Topup
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default TopupWalletPage;
